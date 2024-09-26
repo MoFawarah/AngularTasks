ac25c519f03aab86d8ff8b9f13c5a38bfe0abcb2
MoFawarah <mfawarah1@gmail.com>
2024-09-26 15:59:01 +0300

    updated

---
+++ AngularProject/angularproject.client/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    CategoryComponent,
+    ProductComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "category", component: CategoryComponent },
+      { path: "product", component: ProductComponent },
+      { path: "about", component: AboutUsComponent },
+      { path: "contact us", component: ContactUsComponent}
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
